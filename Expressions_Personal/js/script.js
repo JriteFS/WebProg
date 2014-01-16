6e07cc80d6f449f924b643dec72f34fd7c8aa137
JriteFS <jrite86@yahoo.com>
2014-01-16 17:33:22 -0600

    added

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,4 +1,12 @@
+//Jacob Ritenour
+//1/16/2014
+//Expressions
 
 
+var babyFood = prompt("How many jars of baby food does your child eat in a day?");
+var perWeek = prompt("How many days a week does your child eat baby food?");
+var perMonth = babyFood * perWeek * 4;
+var perYear = perMonth * 12;
+console.log("According to your answers, your child eats" + " " + perMonth + " " + "per month, and on average eats" + " " + perYear + " " + "jars of baby food per year.")
 
 
