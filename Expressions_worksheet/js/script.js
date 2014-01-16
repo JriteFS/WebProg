83631dd4e88f587530cb3a215c64b5ab34f635c4
JriteFS <jrite86@yahoo.com>
2014-01-16 16:04:46 -0600

    added new folder

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,31 @@
+//Jacob Ritenour
+//01/16/2014
+//Expressions Worksheet
+
+//Dog Years
+var humanAge = a;
+var a = 3;
+var dogAge = a * 7;
+console.log(dogAge);
+
+//slice of pie part 1
+var slicesPerpizza = 8;
+var people = 15;
+var pizzas = 6;
+var slicesPerperson = slicesPerpizza * pizzas/people;
+console.log(slicesPerperson);
+
+//slice of pie part 2
+var remainder = 48 % 15;
+console.log(remainder);
+
+//average shopping bill
+var week1 = 125;
+var week2 = 167;
+var week3 = 102;
+var week4 = 95;
+var week5 = 134;
+var average = (week1 + week2 + week3 + week4 + week5)/5;
+console.log(average);
+
+//discounts
