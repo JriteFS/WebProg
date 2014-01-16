67767441b9006519149addcdb5e30047d9e9148c
JriteFS <jrite86@yahoo.com>
2014-01-16 10:03:27 -0600

    added practice page

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,25 +1,3 @@
-alert("Hello World!");
-
-var age = 27;  // Number variable
-
-var isStudent = false;  //Boolean Variable
-
-var studentName = "Jacob";
-
-var phrase = 'I don\'t know \n You\'ll have to speak with the professor';
-
-//Arrays
-
-var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //index of 0,1,2,3
-
-console.log(muppetNames[0]); //depends on where you place each notation
-
-muppetNames[1] = "Rizzo"; // this will change index 1 to the name of Rizzo
-
-var num = 2;
-
-console.log(muppetNames[num]); //lets me see what is in the index selected by var num = x;
-
 
 
 
