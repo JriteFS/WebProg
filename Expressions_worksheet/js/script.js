935daf36f918abce6c201aa6a406567f56c56093
JriteFS <jrite86@yahoo.com>
2014-01-22 22:36:00 -0600

    added changes

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,39 +1,39 @@
 //Jacob Ritenour
 //01/16/2014
-//Expressions Worksheet
+//Conditionals Worksheet
 
-//Dog Years
-var humanAge = 3;
-var dogAge = humanAge * 7;
-console.log("Sparky is" + " " + humanAge + " " + "human years old which is" + " " + dogAge + " " + "in dog years.");
+//Hot Enough?
+//Is it hot enough to go to the beach?
+var temp = 80;
+//if the temperature is less than 75..
 
-//slice of pie part 1
-var slicesPerPizza = 8;
-var people = 15;
-var pizzas = 6;
-var slicesPerPerson = slicesPerPizza * pizzas/people;
-console.log("Each person at" + " " + slicesPerPerson + " " + "slices of pizza at the party.");
+if(temp < 75){
+    //if its less than 75
+    console.log(“We will go to the beach!”);
+}else{
+    //if it’s greater or equal to 75
+    console.log(“We will go to the movies.”);
+}
 
-//slice of pie part 2
-var remainder = 48 % 15;
-console.log("Sparky got" + " " + remainder + " " + "slices of pizza." );
 
-//average shopping bill
-var week1 = 125;
-var week2 = 167;
-var week3 = 102;
-var week4 = 95;
-var week5 = 134;
-var total = week1 + week2 + week3 + week4 + week5;
-var average = (week1 + week2 + week3 + week4 + week5)/5;
-console.log("You have spent a total of" + " " + total + " " + "on groceries over 5 weeks. That is an average of" + " " + average + " " + "per week.");
+//Weight enough for the heavyweight division
+//Do I weigh enough
+var weight = 285;
+//if i weigh more than 250 lbs
 
-//discounts
-var originalPrice = 299.99;
-var discountPercentage = .47; //47%
-var disPercent = 47;
-var description = "Bread Maker";
-var salesTax = .06; //6%
-var afterDiscount = originalPrice * discountPercentage;
-var withTax = afterDiscount * salesTax + afterDiscount;
-console.log("Your" + " " + description + " " + "was originally" + " $" + originalPrice + ", but after a " + disPercent + "% discount, it is now $" + afterDiscount + " without tax, and $" + withTax + " " + "with tax.")
+if(weight > 285){
+    console.log("The competitor qualifies for the heavyweight division.")
+}else{  //i do not weigh more than 250
+    console.log("The competitor needs to gain some weight!")
+}
+
+//Last chance for gas
+var mpg = 25;
+var tankCapacity = 16;
+var tankReading = .6;
+
+if(tankCapacity * tankReading * mpg > 200){
+    console.log("Yes, you can make it without stopping for gas!")
+}else{
+    console.log("You only have " + tankCapacity * tankReading + "gallons of gas in your tank, better stop for gas now while you can!")
+}
