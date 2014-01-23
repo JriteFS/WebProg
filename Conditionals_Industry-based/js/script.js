6f200c65677e116eab2ffc7d8067ea1586aae217
JriteFS <jrite86@yahoo.com>
2014-01-23 09:53:38 -0600

    added a semicolon to complete the process

--- Conditionals_Industry-based/js/script.js
+++ Conditionals_Industry-based/js/script.js
@@ -9,5 +9,5 @@ var department = prompt("How many people does your dept or office have in it at 
 //how many people do you think it should have in it?
 var required = prompt("In your opinion how many people should your office have to handle the workload?");
 //if the amount of people required is greater than the actual amount or less than the amount a suggestion is made.
-(required >= department) ? console.log("Maybe your should talk to a manager about getting more people in.") : console.log("Having to many people is a waste of resources, maybe you should request more work.")
+(required >= department) ? console.log("Maybe your should talk to a manager about getting more people in.") : console.log("Having to many people is a waste of resources, maybe you should request more work.");
 
