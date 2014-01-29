0279091313e989c850e91b73084399982b9fa2a7
JriteFS <jrite86@yahoo.com>
2014-01-29 14:26:22 -0600

    added Functions.

---
+++ Functions_worksheet/js/script.js
@@ -0,0 +1,26 @@
+//Jacob Ritenour
+//01/29/2014
+//Functions Worksheet
+
+//Example
+//Calculate the area of a rectangle
+var width = 5;
+var height = 6;
+var area = calculateArea(width, height);
+
+private function calculateArea(width, height){
+    return w * h;
+}
+
+console.log("The Area of the Rectangle is " + area);
+
+//Circumference
+var radius = 5;
+var circumference = calcCircumference(radius, 3.14){
+    radius * 3.14;
+console.log("The Circumference of the circle is " + circumference)
+}
+private function calcCircumference(radius, 3.14):Number
+{
+    return radius * 3.14;
+}
