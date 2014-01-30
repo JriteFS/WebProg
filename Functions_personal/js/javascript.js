e8a3142d1678bc1859e04180dc4c9cda852ea4b9
JriteFS <jrite86@yahoo.com>
2014-01-30 00:59:55 -0600

    added

--- Functions_personal/js/javascript.js
+++ Functions_personal/js/javascript.js
@@ -8,19 +8,21 @@
 
 
 alert("This exercise is suggesting you have a rectangle or square yard not odd shaped.");
-var side1 = prompt('How long is the bottom side of your yard in feet?');{
-    while (side1 = '') prompt('How long is the bottom side of your yard in feet?');
+//Gather one side
+var side1 = prompt('How long is the bottom side of your yard in feet?');
+    while (side1 == '') { side1 = prompt('How long is the bottom side of your yard in feet?');
 }
-var side2 = prompt('How long is the left or right side of your yard in feet?');{
-    while (side2 = '') prompt('How long is the left or right side of your yard in feet?');
+//gather perpendicular side
+var side2 = prompt('How long is the left or right side of your yard in feet?');
+    while (side2 == ''){ side2 = prompt('How long is the left or right side of your yard in feet?');
 }
-
+//perform function
 var calcPerimeter = function(side1, side2){
     var perimeter = side1 * 2 + side2 *2;
     return perimeter;
-    console.log(perimeter);
 }
-
+//call function
 calcPerimeter(side1, side2);
 var a = calcPerimeter(side1, side2);
-console.log(a);
+//write it
+console.log("You will need " + a + " feet of fence to cover the perimeter of your backyard");
